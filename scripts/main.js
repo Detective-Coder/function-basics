f6f69520f2a339847235c435eaf8c44b52d5f9d6
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-12 13:50:56 -0500

    finished the takeNumber function

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,31 @@
 
 // You can start with this simpler version that outputs even numbers to the console and ignores odd numbers. You need to refactor this code to meet the requirements above.
 
-for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-  if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
-    console.log("ChickenMonkey"); 
-  } else if (currentNumber % 7 === 0) {
-    console.log("Monkey");
-  } else if (currentNumber % 5 === 0) {
-    console.log("Chicken");
-  } else {
-    console.log(currentNumber);
-  }
+// for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+//   if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+//     console.log("ChickenMonkey"); 
+//   } else if (currentNumber % 7 === 0) {
+//     console.log("Monkey");
+//   } else if (currentNumber % 5 === 0) {
+//     console.log("Chicken");
+//   } else {
+//     console.log(currentNumber);
+//   }
+// }
+
+// A local nightclub is having a Battle of the Bands night in a few months. They expect many bands to sign up because the grand prize is $20,000. To make things easier on the nightclub management, they want each band to be assigned a number so that they can easily keep track of the order in which the bands will perform.
+
+// Your job is to write a function that accepts any band name as an argument. The function will increment a global variable by one each time it is invoked, and return that number, and the band name concatenated together.
+
+let bandNumber = 0;
+
+function takeNumber(bandName) {
+  bandNumber++;
+  return bandNumber + "." + " " + bandName;
 }
+
+const scum = takeNumber("Galactic Scum");
+console.log(scum);  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs");
+console.log(under);  // This should print "2. Underdogs" in the console
